94baf6e69d723aca4d4ff4cf632856820623ba4a
Lupita <lupitarivera8899@gmail.com>
2021-07-22 11:07:27 -0500

    remove naming from divs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
     getShop()
+    getItems()
 })
+
+
+
+// fetching store
 
 function getShop(){
     fetch("http://localhost:3000/api/shops")
     .then(res => res.json())
-    .then(data => {
-    data.forEach(image => {
-        const img = new Shop(image)
-        img.addToDom()
-        
-        
+    .then(data => { 
+    data.forEach(name => {
+        const shopName = new Shop(name)
+        shopName.addToDom()
+    
     })
     })
     .catch(error => console.warn(error))
+
+} 
+
+
+
+        // fetching items 
+function getItems(){
+    fetch("http://localhost:3000/api/shops")
+    .then(res => res.json())
+    .then(data => {
+        data[0].items.forEach(item => {
+            // i think i need to itererate again 
+           
+            const img = new Item(item)
+            img.addToDom1() 
+        
+        })
+    })
+    .catch(error => console.warn(error))
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/src/items.js
@@ -0,0 +1,23 @@
+class Item {
+    constructor({image,title,price}){
+        this.image = image
+        this.title = title
+        this.price = price
+        Item.all.push(this)
+    }
+    static all = []
+
+    addToDom1(){
+        const image = document.querySelector("#image-container")
+        image.innerHTML += this.renderItems()
+    }
+
+    renderItems(){
+        return (`<img src= " ${this.image} " />
+        <h3>${this.title}</h3>
+        <h2>$ ${this.price}</h2>`)
+    }
+
+
+
+}
