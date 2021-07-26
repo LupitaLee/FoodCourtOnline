5c24310b10df30be915ba186a2421403b88086d5
Lupita <lupitarivera8899@gmail.com>
2021-07-26 17:20:09 -0500

    add post request and render to dom functions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,59 +1,121 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getShop()
+   getShop()
     getItems()
-    createSushi()
+    
+    postItems()
 })
 
 
 
-// fetching store
+//--------------- new code ---------- fetch and render shop
+
+
 
 function getShop(){
     fetch("http://localhost:3000/api/shops")
     .then(res => res.json())
-    .then(data => { 
-    data.forEach(name => {
-        const shopName = new Shop(name)
-        shopName.addToDom()
-    
-    })
-    })
+    .then(data => renderShop(data))
     .catch(error => console.warn(error))
+}
 
-} 
+function renderShop(data){
+   
+    const titleName = document.querySelector("#name")
+    data.forEach(shopName => {
+        
+    
+    const h1 = document.createElement("h1")
+   
+    h1.innerHTML =  shopName.name
+    titleName.append(h1)
+    }); 
+    }
 
 
 
-        // fetching items 
+    
+// ------ new code fetch and render items -----
+
+
 function getItems(){
     fetch("http://localhost:3000/api/shops")
     .then(res => res.json())
-    .then(data => {
-        data[0].items.forEach(item => {
-            // i think i need to itererate again 
-           
-            const img = new Item(item)
-            img.addToDom1() 
-        
-        })
-    })
+    .then(data => (
+        data[0].items.forEach(item => renderItem(item)))
     .catch(error => console.warn(error))
+        
+    )}
+
+
+const image = document.querySelector("#image-container")
+
+
+
+function renderItem(item){
+    const img = document.createElement("img")
+    img.src = item.image
+    
+
+    const h3 = document.createElement("h3")
+    h3.innerHTML = item.title
+
+    const h4 = document.createElement("h4")
+    h4.innerHTML = `$ ${item.price}`
+
+    image.append(img, h3, h4)
 }
 
 
 
-// create items 
-const createform = document.querySelector("#item-post-form")
+
+//---------- create form ---------
+const createForm = document.querySelector("#item-post-form")
 
 
-function createSushi(){
-    createform.addEventListener("submit", function(e){
-        e.preventDefault()
-    })
+
+//------------------------post items and render -------
+
+
+//---------   -----------------
+
+
+
+function postItems(){
+        // this should be its own function
+        createForm.addEventListener("submit", function(e){
+          
+            e.preventDefault()
+            const form = e.target
+            const titleInput = form.querySelector("#title-input")
+            const imageInput = form.querySelector("#image-input")
+            //--------
+     
+            // take this values and sent to db to create a new roll
+            fetch("http://localhost:3000/api/shops/1/items", {
+                method:"POST",
+                headers: {
+                    "Content-Type": "application/json",// we sennding it thru as jason to our back end
+                    "Accept": "application/json" // we accepting json back from our backend
+                },
+                body: JSON.stringify({
+                 
+                   image: imageInput.value,
+                    title:titleInput.value,
+                    price: "10.50" // need to see if how im going to add a flat fee with out using user input
+                })
+            })
+            .then(resp => resp.json())// this is the only time we get an implicit return in js written on one line arrow
+            .then(item => {
+                console.log('Success:', item)
+              
+             
+                renderItem(item)
+
+              })
+              .catch((error) => {
+                console.error('Error:', error)
+              })
+        })
 }
-
-
-
-
-
-
+     
+    
